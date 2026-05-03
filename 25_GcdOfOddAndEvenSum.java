99976095c051c825d7d6ca2933a627b2da2c8098
Yash Upase <yashupase777@gmail.com>
2026-05-03 22:32:51 +0530

    Add problem description for GCD of Odd and Even Sums
    Added problem statement and example for GCD of odd and even sums.

--- 25_GcdOfOddAndEvenSum.java
+++ 25_GcdOfOddAndEvenSum.java
@@ -1,3 +1,16 @@
+//Problem numbber: 3658. GCD of Odd and Even Sums
+// Example 1:
+
+// Input: n = 4
+
+// Output: 4
+
+// Explanation:
+
+// Sum of the first 4 odd numbers sumOdd = 1 + 3 + 5 + 7 = 16
+// Sum of the first 4 even numbers sumEven = 2 + 4 + 6 + 8 = 20
+// Hence, GCD(sumOdd, sumEven) = GCD(16, 20) = 4.
+
 class Solution {
     public int gcdOfOddEvenSums(int n) {
        int oddSum = n*n;
