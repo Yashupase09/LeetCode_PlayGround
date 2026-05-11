ee26b2386bca7c467ac238f40edf6913af83fae0
Yash Upase <yashupase777@gmail.com>
2026-05-11 23:41:56 +0530

    Implement intersection of two arrays

---
+++ 29_IntersectionOfTwoArrays.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int[] intersection(int[] nums1, int[] nums2) {
+        Set<Integer> set1 = new HashSet<>();
+        for (int n : nums1) {
+            set1.add(n);
+        }
+
+        Set<Integer> result = new HashSet<>();
+        for (int n : nums2) {
+            if (set1.contains(n)) {
+                result.add(n);
+            }
+        }
+
+        int[] ans = new int[result.size()];
+        int i = 0;
+        for (int n : result) {
+            ans[i++] = n;
+        }
+
+        return ans;
+    }
+}
