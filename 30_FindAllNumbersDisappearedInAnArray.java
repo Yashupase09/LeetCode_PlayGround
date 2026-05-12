a7e503d2c82081baa4d8fab88fa66bb17beece21
Yash Upase <yashupase777@gmail.com>
2026-05-12 23:03:23 +0530

    Add solution to find disappeared numbers in array
    Implement method to find all numbers that are missing from the array.

---
+++ 30_FindAllNumbersDisappearedInAnArray.java
@@ -0,0 +1,25 @@
+/*Example 1:
+
+Input: nums = [4,3,2,7,8,2,3,1]
+Output: [5,6]
+Example 2:
+
+Input: nums = [1,1]
+Output: [2]*/
+
+class Solution {
+    public List<Integer> findDisappearedNumbers(int[] nums) {
+        Set<Integer> set = new HashSet<>();
+        for (int num : nums) {
+            set.add(num);
+        }
+
+        List<Integer> result = new ArrayList<>();
+        for (int i = 1; i <= nums.length; i++) {
+            if (!set.contains(i)) {
+                result.add(i);
+            }
+        }
+        return result;
+    }
+}
